fa86eda605dcbd610a501148aa06845241a1ca8d
GPdIProgram <anugerahinformation@gmail.com>
2025-11-02 11:35:00 +0700

    Add files via upload

---
+++ Project/demo/src/main/java/qrcode/Main.java
@@ -0,0 +1,13 @@
+package qrcode;
+
+import com.google.zxing.BarcodeFormat;;
+
+public class Main {
+
+    int test = 1;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("test");
+    }
+}
